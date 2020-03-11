9d8a70da01e22aec7d4ef6673beb35deae6068a4
JuicyJerry <ju2cyjerry@gmail.com>
2020-03-11 23:21:35 +0900

    객체 지향 프로그래밍 , ch1 객체를 만드는 법, __init__
    __init__

--- OOP(#1)/initailize 메소드.py
+++ OOP(#1)/initailize 메소드.py
@@ -24,3 +24,31 @@ print(user2.name, user2.email, user2.password)
 print(user3.name, user3.email, user3.password)
 print(user4.name, user4.email, user4.password)
 
+
+# magic method(특수 메소드: 특정 상황에서 자동으로 호출되는 메소드)
+# __init__ : 인스턴스가 생성될 때 자동으로 호출
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+# https://thebook.io/006888/partxt/xa/04/
+
+user1 = User("Youn g","young@codeit.kr", "123456")
+# 1. User 인스턴스 생성
+# 2. __init__ 메소드 자동 호출
+# 2-1. self <- User인스턴스
+# 2-2. 나머지 파라미터들이 순서대로 들어간다.
+# 2-3. 인스턴스 변수들의 초깃값 설정
+
+
+user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
+
+user3 = User("Taeho", "taeho@codeit.kr", "123abc")
+
+user4 = User("Lisa", "lisa@codeit.kr", "abc123")
+
+print(user1.name, user1.email, user1.password)
+print(user2.name, user2.email, user2.password)
+print(user3.name, user3.email, user3.password)
+print(user4.name, user4.email, user4.password)
